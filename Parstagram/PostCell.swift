7fc376a6f79f55a9b809b0079a0414804a681252
Fung Yung <fungyung1225@gmamil.com>
2019-03-20 18:59:11 -0400

    add post cell controller link tableView

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Fung on 3/20/19.
+//  Copyright © 2019 fungyung. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
